20ac8ffa1cfd5df51678627a95a5daeb41ead681
Jimmy Figueroa <av_jfigueroa@idcontractors.net>
2019-05-20 19:16:34 -0600

    reset index and sorted

--- src/clean_epl2017.py
+++ src/clean_epl2017.py
@@ -1,10 +1,20 @@
 # load dataset
 import pandas as pd 
 import yaml
+import re
 
 CONFIG_PATH='./config/config.yml'
 _config = yaml.load( open( CONFIG_PATH ))
 dataset = _config['dev']['file_path_name']
 df = pd.read_csv(dataset)
 
-print("succeeded !")
+# transform home/away as pivot fields
+# introducing pandas's «melt»
+df_melted = df.melt(id_vars=['Round Number', 'Result', 'Date', 'Location'])
+
+# sort the melted df
+columns = ['Round Number', 'Date', 'Location', 'variable', 'value', 'Result']
+df_sorted = df_melted[columns].sort_values(by=['Round Number', 'Date'])
+
+# reset index
+df_sorted = df_sorted.reset_index(drop=True)
